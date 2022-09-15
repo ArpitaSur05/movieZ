29c7dac9cc68022f818c45e51315b2e311f80f04
Arpita <surarpita01@gmail.com>
2022-09-16 00:45:33 +0530

    refactor nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav-logo">Movies-Catalog</div>{" "}
+      <button className="button-add">Add</button>
+    </nav>
+  );
+};
+export default Nav;
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
 import "../styles/Nav.css";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <div className="nav-logo">Movies-Catalog</div>{" "}
-        <button className="button-add">Add</button>
-      </nav>
+      <Nav />
       <Outlet />
     </>
   );
